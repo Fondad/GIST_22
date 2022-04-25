f692f15497ba4e9e75c57de3413b2349fc21ac8c
Vsevolod Demidov <fondad@ukr.net>
2022-04-25 12:00:58 +0300

    In home template added Articles

--- mygis/views.py
+++ mygis/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mygis.models import Articles
 
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Hello world!")
-    return render(request, 'mygis/home.html')
+    # return render(request, 'mygis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'mygis/home.html', context)
+
